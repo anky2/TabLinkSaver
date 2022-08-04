f3eba2313ff05d3411708f7e429672d0d84d794d
Ankit Yadav <ankityadav200124@gmail.com>
2022-08-04 23:05:53 +0530

    Added list items

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 
 
-let myLeads=[]
-let inputText = document.getElementById("input-el")
-
+let myLeads=["ankit","yadav"]
+let inputText = document.getElementById("input-el");
+let ulEl = document.getElementById("ul-El");
 let btnClick=document.getElementById("input-btn");
 
 btnClick.addEventListener("click",function(){
     myLeads.push(inputText.value);
-    console.log(myLeads);
+    
 })
+
+for(let i=0; i<myLeads.length; i++){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
